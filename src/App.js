6f1bad7de651c9a5e9216bc72d90bbaf21b4e67b
martsDev <martssilvapro@gmail.com>
2022-02-22 20:29:52 -0300

    adiciona input basico

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 
 function App() {
   return (
-    <div className="App">
-      Hi
+    <div class="container">
+      <div class="searchbox">
+        <input type="text" class="searchbox-input" placeholder="Search..." />
+        <i class="search-icon fas fa-search"></i>
+      </div>
     </div>
   );
 }
