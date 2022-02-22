fff8100567e515b894e52e1b29161543fd4df210
martsDev <martssilvapro@gmail.com>
2022-02-22 20:42:31 -0300

    adiciona icon e estilo personalizado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import "./App.css";
 
 function App() {
   return (
     <div class="container">
       <div class="searchbox">
-        <input type="text" class="searchbox-input" placeholder="Search..." />
-        <i class="search-icon fas fa-search"></i>
+        <input type="text" className="searchbox-input" placeholder="Search..." />
+        <FontAwesomeIcon icon={faMagnifyingGlass} className="search-icon" />
       </div>
     </div>
   );
