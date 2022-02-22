671843976d7e16ab6ca1f47c3f6da822ba04f0f1
martsDev <martssilvapro@gmail.com>
2022-02-22 20:48:51 -0300

    adiciona data file

---
+++ src/data.js
@@ -0,0 +1,14 @@
+export default data = [
+    {
+        book: "Lord Of The Rings",
+        author: "J.R.R Tolkien"
+    },
+    {
+        book: "Filoteia",
+        author: "São Francisco de Sales"
+    },
+    {
+        book: "The Hitchhiker's Guide to the Galaxy",
+        author: "Douglas Adams"
+    },
+]
