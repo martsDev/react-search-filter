dcd990b65064870bcbc0a0cad10367dd39081c5f
martsDev <martssilvapro@gmail.com>
2022-02-22 21:26:10 -0300

    implementa filtro basico

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,30 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import data from './data';
 import "./App.css";
+import { useState } from 'react';
 
 function App() {
+
+  const [item, setItem] = useState("");
+
+
   return (
     <div>
-    <div class="container">
-      <div class="searchbox">
-        <input type="text" className="searchbox-input" placeholder="Search..." />
-        <FontAwesomeIcon icon={faMagnifyingGlass} className="search-icon" />
+      <div className="container">
+        <div className="searchbox">
+          <input 
+            type="text" 
+            className="searchbox-input" 
+            placeholder="Search..."
+            onChange={(e) => setItem(e.target.value)} 
+          />
+          <FontAwesomeIcon icon={faMagnifyingGlass} className="search-icon" />
+        </div>
       </div>
-    </div>
-    <div className='container-list'>
-      <ul className='list'>
-          <li className='listItem'>{data[0].book}</li>
-          <li className='listItem'>{data[1].book}</li>
-          <li className='listItem'>{data[2].book}</li>
+      <div className='container-list'>
+        <ul className='list'>
+          {data.filter((title) => title.book.toLowerCase().includes(item)
+          ).map((title) => <li>{title.book}</li>)}
         </ul>
       </div>
     </div>
